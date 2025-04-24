0f14980b34d4d31a78c330fb77cc033a6df83525
Faseeha <naazfaseeha@gmail.com>
2025-04-24 12:00:51 +0530

    Update MySqlDBConnection.java

--- SMS/src/DATASOURCE/MySqlDBConnection.java
+++ SMS/src/DATASOURCE/MySqlDBConnection.java
@@ -5,7 +5,7 @@ public class MySqlDBConnection {
 	public MySqlDBConnection() throws Exception {
 		Class.forName("com.mysql.jdbc.Driver");
 		System.out.println("Driver Loaded");
-		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/techtest","root","Faseeha#150804");
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/techtest","root","root");
 		System.out.println("Connection Established");
 	}
 	public Connection getConnection() {
